8ab36a2d54e756e4dbfce50eed7c2c3ec60ca19f
Vitor Britto <code@vitorbritto.com.br>
2015-07-05 11:36:00 +0100

    Update package.json

---
+++ index.js
@@ -0,0 +1,56 @@
+/*!
+ * Copyright 2015, All Rights Reserved.
+ *
+ * Code licensed under the MIT License:
+ * http://vitorbritto.mit-license.org/
+ *
+ * Author: Vitor Britto <code@vitorbritto.com.br>
+ */
+
+
+(function(window, document, undefined) {
+
+    'use strict';
+
+    var ioQuery = (function() {
+
+        var extract = extract || {};
+
+        // EXTRACT METHOD
+        extract.query = function(url) {
+
+            var qparts,
+                qpart,
+                i       = 0,
+                qparams = {},
+                parts   = (url || '').split('?');
+
+            if (parts.length <= 1) {
+                return qparams;
+            } else {
+                qparts = parts[1].split('&');
+                for (i in qparts) {
+                    if (qparams.hasOwnProperty) {
+                        qpart = qparts[i].split('=');
+                        qparams[decodeURIComponent(qpart[0])] = decodeURIComponent(qpart[1] || '');
+                    }
+                }
+                return qparams;
+            }
+
+        };
+
+        // INITIALIZE MODULE
+        return {
+            parse: extract.query
+        };
+
+    })();
+
+    // Global
+    window.ioQuery = ioQuery;
+
+})(window, document);
+
+
+ioQuery.init(window.location.href);
